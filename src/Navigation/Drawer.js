a575ffabe4e47471be93e822824ac590724d851a
Junhaochia <newboy.chia5@gmail.com>
2023-06-17 21:20:11 +0800

    Updated Drawer Style

--- src/Navigation/Drawer.js
+++ src/Navigation/Drawer.js
@@ -1,17 +1,37 @@
+import { View, Text } from 'react-native';
+
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import NewScreen from '../screens/NewScreen';
 import LoginScreen from '../screens/LoginScreen';
 
+const DrawerTabs = [{
+  name: "News",
+  component: NewScreen
+},
+{
+  name: "Login",
+  component: LoginScreen
+}]
+
 const Drawer = createDrawerNavigator();
 
 function NavigationDrawer() {
-    return (
-        <Drawer.Navigator useLegacyImplementation>
-          <Drawer.Screen name="Login" component={LoginScreen} />
-          <Drawer.Screen name="News" component={NewScreen} />
-        </Drawer.Navigator>
-    );
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#FFFFFFF8',
+          overlayColor: 'transparent'
+        },
+        headerTitleAlign: 'center',
+      }}
+      useLegacyImplementation>
+      {DrawerTabs.map((screen) => (<Drawer.Screen name={screen.name} key={screen} options={{
+        drawerIcon: ({ focused, color, size }) => (<View style={{ width: '100%' }}><Text style={{ color, textAlign: 'center' }}>{screen.name}</Text></View>)
+      }} component={screen.component} />))}
+    </Drawer.Navigator>
+  );
 }
 
 export default NavigationDrawer;
