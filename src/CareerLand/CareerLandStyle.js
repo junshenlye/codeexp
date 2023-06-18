1e8b6649c83b613ed06de3a1ca905cb543b93409
ItsDaMeMario <junshenlye@gmail.com>
2023-06-19 01:02:46 +0800

    ''

--- src/CareerLand/CareerLandStyle.js
+++ src/CareerLand/CareerLandStyle.js
@@ -21,6 +21,33 @@ export const styles = StyleSheet.create({
         position: 'absolute',
         width: '100%',
         height: '100%',
-    }
-
+      },
+      overlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      },
+      content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      checkpointContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      checkpoint: {
+        width: 24,
+        height: 24,
+        borderRadius: 12,
+        backgroundColor: 'white',
+        marginHorizontal: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      checkpointText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'black',
+      },
 });
--- src/screens/CareerLandScreen.js
+++ src/screens/CareerLandScreen.js
@@ -3,13 +3,31 @@ import { View, Text, ImageBackground } from 'react-native';
 import { styles } from '../CareerLand/CareerLandStyle';
 import image from '../../assets/image.png';
 
-
 const CareerLandScreen = () => {
-  return (
-      <View style={styles.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-    </ImageBackground>
+  const handleCheckpointPress = (checkpoint) => {
+    // Code to handle checkpoint press
+    console.log(`Checkpoint ${checkpoint} pressed!`);
+  };
+
+  const renderCheckpoints = () => {
+    const checkpoints = [1, 2, 3]; // Example checkpoints
+
+    return checkpoints.map((checkpoint) => (
+      <View key={checkpoint} style={styles.checkpoint}>
+        <Text style={styles.checkpointText}>{checkpoint}</Text>
       </View>
+    ));
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.overlay} />
+        <View style={styles.content}>
+          <View style={styles.checkpointContainer}>{renderCheckpoints()}</View>
+        </View>
+      </ImageBackground>
+    </View>
   );
 };
 
