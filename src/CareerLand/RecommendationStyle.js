e328e819a18923809465bbdf32ad593a107175eb
ItsDaMeMario <junshenlye@gmail.com>
2023-06-19 21:54:04 +0800

    Updated

---
+++ src/CareerLand/RecommendationStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headerContainer: {
+    padding: 16,
+  },
+  backButton: {
+    padding: 8,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
+
+export default styles;
--- src/screens/RecommendationScreen.js
+++ src/screens/RecommendationScreen.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import styles from '../CareerLand/RecommendationStyle'; // Import the styles object from styles.js
 
 const RecommendationScreen = ({ navigation }) => {
-
   const handleBackPress = () => {
     navigation.goBack(); // Go back to the previous screen
   };
 
   return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
+    <View style={styles.container}>
       {/* Header */}
-      <View style={{ padding: 16 }}>
-        <TouchableOpacity onPress={handleBackPress}>
+      <View style={styles.headerContainer}>
+        <TouchableOpacity onPress={handleBackPress} style={styles.backButton}>
           <Text>Back</Text>
         </TouchableOpacity>
       </View>
 
       {/* Content */}
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>This is the new screen!</Text>
+      <View style={styles.contentContainer}>
+        <Text style={styles.text}>This is the new screen!</Text>
       </View>
     </View>
   );
 };
 
 export default RecommendationScreen;
+
+
+
+
