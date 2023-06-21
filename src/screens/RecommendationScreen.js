bf7cce46fea7b7a6630d59a5371a8ce6905e1a86
ItsDaMeMario <junshenlye@gmail.com>
2023-06-21 13:58:27 +0800

    'updated'

--- src/screens/RecommendationScreen.js
+++ src/screens/RecommendationScreen.js
@@ -1,33 +1,66 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ScrollView, Linking } from 'react-native';
 import styles from '../CareerLand/RecommendationStyle'; // Import the styles object from styles.js
-import image from '../../assets/back.png';
-import { ScrollView } from 'react-native-gesture-handler';
 
 const RecommendationScreen = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <ScrollView>
-                <View style={styles.contentContainer}>
-                    <Text style={styles.text}>Description:</Text>
-                </View>
-            </ScrollView>
+  const handleURLPress = (url) => {
+    Linking.openURL(url);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.contentContainer}>
+          <Text style={styles.text}>Description:</Text>
         </View>
-    );
+
+        
+        <View style={styles.coursecontainer}>
+          {data.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => handleURLPress(item.url)}
+              style={styles.itemContainer}
+            >
+              <Text style={styles.title}>{item.title}</Text>
+              <Image source={item.image} style={styles.image} />
+              <Text style={styles.text}>Next Course Available on:</Text>
+              <Text style={styles.courseDate}>{item.courseDate}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
 
 const data = [
   {
-    image: require('./path/to/image1.jpg'),
+    image: require('../../assets/courseimage1.jpg'),
     title: 'Course 1',
     courseDate: 'June 1, 2023',
-    url: 'https://example.com/course1',
+    url: 'https://www.google.com',
   },
+
   {
-    image: require('./path/to/image2.jpg'),
-    title: 'Course 2',
-    courseDate: 'June 15, 2023',
-    url: 'https://example.com/course2',
+    image: require('../../assets/courseimage1.jpg'),
+    title: 'Course 1',
+    courseDate: 'June 1, 2023',
+    url: 'https://www.google.com',
+  },
+
+  {
+    image: require('../../assets/courseimage1.jpg'),
+    title: 'Course 1',
+    courseDate: 'June 1, 2023',
+    url: 'https://www.google.com',
+  },
+
+  {
+    image: require('../../assets/courseimage1.jpg'),
+    title: 'Course 1',
+    courseDate: 'June 1, 2023',
+    url: 'https://www.google.com',
   },
 ];
 
