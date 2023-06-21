7fe708ecffbd29158e11c7b5378fc11b210dad06
Junhaochia <newboy.chia5@gmail.com>
2023-06-22 00:40:01 +0800

    update profile screen

--- src/Dashboard/ObjectivesPanel.js
+++ src/Dashboard/ObjectivesPanel.js
@@ -13,178 +13,179 @@ const paddingTopSpacer = 10;
 
 
 const ObjectiveStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundColor: colorDebug && '#7CA1B4',
-    width: '100%',
-    height: '100%',
-  },
-  item: {
-    backgroundColor: colorDebug && '#7cb48f',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		backgroundColor: colorDebug && '#7CA1B4',
+		width: '100%',
+		height: '100%',
+	},
+	item: {
+		backgroundColor: colorDebug && '#7cb48f',
+	},
 });
 
 function Objective({ item }) {
-  const [statsCount, setStatsCount] = useState(item.default);
+	const [statsCount, setStatsCount] = useState(item.default);
 
-    useEffect(() => {
-        async function resetGoals() {
-            await updateStats(item.name, item.default);
-        }
-        // resetGoals();
+	useEffect(() => {
+		async function resetGoals() {
+			await updateStats(item.name, item.default);
+		}
+		// resetGoals();
 
-    async function updatePanel() {
-      setStatsCount(
-        Math.min(await getStats(item.name, item.default), item.max)
-      );
-    }
+		async function updatePanel() {
+			setStatsCount(
+				Math.min(await getStats(item.name, item.default), item.max)
+			);
+		}
 
-    const refreshInterval = setInterval(updatePanel, 500);
+		const refreshInterval = setInterval(updatePanel, 500);
 
-    return () => {
-      clearInterval(refreshInterval);
-    };
-  }, [item.default, item.name, statsCount, item.max]);
+		return () => {
+			clearInterval(refreshInterval);
+		};
+	}, [item.default, item.name, statsCount, item.max]);
 
-  const fill = Math.round((statsCount / item.max) * 100);
+	const fill = Math.round((statsCount / item.max) * 100);
 
-  const tintColor = fill == 100 ? '#4C5EB7' : '#8590C8';
-  return (
-    <>
-      <View style={ObjectiveStyle.container}>
-        <View style={ObjectiveStyle.item}>
-          <AnimatedCircularProgress
-            // Todo Size based on Screen Size
-            size={65}
-            width={8}
-            fill={fill % 25 == 0 ? fill - 1 : fill} // Bug spacing of 25 will not have a flat linecap on one of the ends
-            tintColor={tintColor}
-            backgroundColor="#C2CAF2"
-            rotation={fill % 25 == 0 ? 1 : 0}
-            lineCap="round"
-            fillLineCap="round"
-            duration={2000}>
-            {() => (
-              <Text style={{ fontWeight: 'bold' }}>
-                {statsCount}/{item.max}
-              </Text>
-            )}
-          </AnimatedCircularProgress>
-        </View>
-        <View style={ObjectiveStyle.item}>
-          <Text style={{ paddingTop: paddingTopSpacer }}>{item.name}</Text>
-        </View>
-      </View>
-    </>
-  );
+	const tintColor = fill == 100 ? '#4C5EB7' : '#8590C8';
+	return (
+		<>
+			<View style={ObjectiveStyle.container}>
+				<View style={ObjectiveStyle.item}>
+					<AnimatedCircularProgress
+						// Todo Size based on Screen Size
+						size={65}
+						width={8}
+						fill={fill % 25 == 0 ? fill - 1 : fill} // Bug spacing of 25 will not have a flat linecap on one of the ends
+						tintColor={tintColor}
+						backgroundColor="#C2CAF2"
+						rotation={fill % 25 == 0 ? 1 : 0}
+						lineCap="round"
+						fillLineCap="round"
+						duration={2000}>
+						{() => (
+							<Text style={{ fontWeight: 'bold' }}>
+								{statsCount}/{item.max}
+							</Text>
+						)}
+					</AnimatedCircularProgress>
+				</View>
+				<View style={ObjectiveStyle.item}>
+					<Text style={{ paddingTop: paddingTopSpacer }}>{item.name}</Text>
+				</View>
+			</View>
+		</>
+	);
 }
 
 const PanelTitleStyle = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: colorDebug && 'blue',
-  },
-  leftContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    backgroundColor: colorDebug && 'green',
-  },
-  centerContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    backgroundColor: colorDebug && 'yellow',
-  },
-  rightContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    verticalAlign: 'middle',
-    backgroundColor: colorDebug && 'red',
-  },
-  coinContainer: {
-    flexDirection: 'row',
-    borderRadius: 25,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    verticalAlign: 'middle',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    backgroundColor: '#ECECEC',
-  },
-  coinIcon: {
-    width: 20,
-    height: 20,
-    marginRight: 5,
-  },
+	titleContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: colorDebug && 'blue',
+	},
+	leftContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		backgroundColor: colorDebug && 'green',
+	},
+	centerContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		backgroundColor: colorDebug && 'yellow',
+	},
+	rightContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		verticalAlign: 'middle',
+		backgroundColor: colorDebug && 'red',
+	},
+	coinContainer: {
+		flexDirection: 'row',
+		borderRadius: 25,
+		paddingHorizontal: 10,
+		paddingVertical: 5,
+		verticalAlign: 'middle',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		backgroundColor: '#ECECEC',
+	},
+	coinIcon: {
+		width: 20,
+		height: 20,
+		marginRight: 5,
+	},
 });
 
 const PanelStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    backgroundColor: colorDebug && 'pink',
-  },
-  item: {
-    backgroundColor: colorDebug && 'yellow',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		backgroundColor: colorDebug && 'pink',
+	},
+	item: {
+		backgroundColor: colorDebug && 'yellow',
+	},
 });
 
-export default function ObjectivesPanel({ navigation }) {
-  return (
-    <View style={{ width: '100%', height: '100%', paddingHorizontal: 10 }}>
-      {/* Objective Panel Title and Money */}
-      <>
-        <View style={PanelTitleStyle.titleContainer}>
-          <View style={PanelTitleStyle.leftContainer}>
-            {/* Left empty for alignment */}
-          </View>
-          <View>
-            <Text
-              style={{
-                fontSize: 16,
-                fontWeight: 'bold',
-              }}>
-              Daily Goals
-            </Text>
-          </View>
-          <View style={PanelTitleStyle.rightContainer}>
-            <TouchableOpacity
-              style={PanelTitleStyle.coinContainer}
-              onPress={() => navigation.navigate('Reward Shop')}>
-              <Image
-                style={PanelTitleStyle.coinIcon}
-                source={require('../../assets/coin.png')}
-              />
-              <Text
-                style={{
-                  fontWeight: 'bold',
-                }}>
-                100
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </>
-      <View style={{ paddingTop: paddingTopSpacer }} />
-      {/* Objective List */}
-      <>
-        <TouchableOpacity style={PanelStyle.container} onPress={() => navigation.navigate('Challenges')}>
-          {ObjectivesArray.map((objectiveItem) => (
-            <View key={objectiveItem.name} style={PanelStyle.item}>
-              <Objective item={objectiveItem} />
-            </View>
-          ))}
-        </TouchableOpacity>
-      </>
-    </View>
-  );
+export default function ObjectivesPanel({ navigation, showBalance = false, headerTitleStyle }) {
+	return (
+		<View style={{ width: '100%', height: '100%', paddingHorizontal: 10 }}>
+			{/* Objective Panel Title and Money */}
+			<>
+				<View style={PanelTitleStyle.titleContainer}>
+					<View style={PanelTitleStyle.leftContainer}>
+						{/* Left empty for alignment */}
+					</View>
+					<View>
+						<Text
+							style={{
+								fontSize: 16,
+								...headerTitleStyle,
+								fontWeight: 'bold',
+							}}>
+							Daily Goals
+						</Text>
+					</View>
+					{showBalance &&
+						(<View style={PanelTitleStyle.rightContainer}>
+							<TouchableOpacity
+								style={PanelTitleStyle.coinContainer}
+								onPress={() => navigation.navigate('Reward Shop')}>
+								<Image
+									style={PanelTitleStyle.coinIcon}
+									source={require('../../assets/coin.png')}
+								/>
+								<Text style={{ fontWeight: 'bold' }}>100</Text>
+							</TouchableOpacity>
+						</View>) ||
+						(<View style={PanelTitleStyle.rightContainer}>
+							{/* Left empty for alignment */}
+						</View>)
+					}
+				</View>
+			</>
+			<View style={{ paddingTop: paddingTopSpacer }} />
+			{/* Objective List */}
+			<>
+				<TouchableOpacity style={PanelStyle.container} onPress={() => navigation.navigate('Challenges')}>
+					{ObjectivesArray.map((objectiveItem) => (
+						<View key={objectiveItem.name} style={PanelStyle.item}>
+							<Objective item={objectiveItem} />
+						</View>
+					))}
+				</TouchableOpacity>
+			</>
+		</View>
+	);
 }
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,22 +1,56 @@
 import { useEffect } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
+import { ImageBackground, View, Text, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
+import { useHeaderHeight } from '@react-navigation/elements';
 
 import ObjectivesPanel from '../Dashboard/ObjectivesPanel';
 
 const colorDebug = false;
 
+const styles = StyleSheet.create({
+    textTitle: {
+        color: '#807DC3',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textShadowColor: 'rgba(0, 0, 0, 0.05)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 10
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    coinContainer: {
+        flexDirection: 'row',
+        borderRadius: 25,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        verticalAlign: 'middle',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        borderColor: "rgba(0,0,0,0.2)",
+        borderWidth: 0.4
+    },
+    coinIcon: {
+        width: 20,
+        height: 20,
+        marginRight: 5,
+    },
+    badgeIcon: {
+        width: 75,
+        height: 75,
+    },
+});
+
 const cardStyle = StyleSheet.create({
     item: {
         flex: 1,
-        backgroundColor: 'white',
-        borderRadius: 20,
         padding: 10,
         marginBottom: 10,
-        shadowColor: 'gray',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.3,
-        shadowRadius: 2,
-        elevation: 2,
+        borderRadius: 35,
+        backgroundColor: 'rgba(255,255,255,0.05)',
+        borderColor: "rgba(0,0,0,0.2)",
+        borderWidth: 0.4,
     },
 });
 
@@ -67,11 +101,11 @@ function StatsAndCharacterPanel() {
         <View style={{ ...rowFlexBoxStyle.item, width: "50%" }}>
             <StatsPanel />
         </View>
-        <View style={{ ...rowFlexBoxStyle.item, width: "50%"}}>
-                <Image
-                    style={{width: 141, height: 280, margin: 10}}
-                    source={require('../../assets/profile/character.png')}
-                />
+        <View style={{ ...rowFlexBoxStyle.item, width: "50%" }}>
+            <Image
+                style={{ width: 141, height: 280, margin: 10 }}
+                source={require('../../assets/profile/character.png')}
+            />
         </View>
     </View>)
 }
@@ -81,30 +115,60 @@ export default function ProfileScreen({ navigation }) {
     // Update Navigation Title
     useEffect(() => {
         navigation.setOptions({
+            headerTransparent: true,
             title: 'James',
-            headerStyle: {
-                backgroundColor: '#92B0FF',
+            headerTitleStyle: {
+                color: '#C97272',
+                fontSize: 20,
+                fontWeight: 'bold',
+                textShadowColor: 'rgba(0, 0, 0, 0.05)',
+                textShadowOffset: { width: -1, height: 1 },
+                textShadowRadius: 10
             },
+            headerRight: () => (
+                <View style={{ ...styles.coinContainer, marginRight: 20 }}>
+                    <Image
+                        style={styles.coinIcon}
+                        source={require('../../assets/coin.png')}
+                    />
+                    <Text
+                        style={{
+                            fontWeight: 'bold',
+                        }}>
+                        100
+                    </Text>
+                </View>
+            )
         });
     }, []);
 
     return (
-        <ScrollView style={{ margin: 20 }}>
-            <View style={{ paddingHorizontal: 10 }}>
-                <StatsAndCharacterPanel />
-            </View>
-            <View style={{ ...cardStyle.item }}>
-                <ObjectivesPanel navigation={navigation} />
-            </View>
-            <View style={{ ...cardStyle.item }}>
-                <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Recent Badges</Text>
-                <Text> </Text>
-                <Text>2 NO</Text>
-                <Text>2 NO</Text>
-                <Text>2 NO</Text>
-                <Text>2 NO</Text>
-                <Text>2 NO</Text>
-            </View>
-        </ScrollView>
+        <ImageBackground source={require('../../assets/backdrop/profile.png')} resizeMode="cover" style={styles.image}>
+            <ScrollView style={{ marginTop: useHeaderHeight(), marginHorizontal: 20 }}>
+                <View style={{ paddingHorizontal: 10 }}>
+                    <StatsAndCharacterPanel />
+                </View>
+                <View style={{ ...cardStyle.item }}>
+                    <ObjectivesPanel navigation={navigation} headerTitleStyle={styles.textTitle} />
+                </View>
+                <View style={{ ...cardStyle.item }}>
+                    <Text style={{ ...styles.textTitle, textAlign: 'center' }}>Recent Badges</Text>
+                    <View style={{paddingTop: 20, ...rowFlexBoxStyle.container}}>
+                        <Image
+                            style={styles.badgeIcon}
+                            source={require('../../assets/badge.png')}
+                        />
+                        <Image
+                            style={styles.badgeIcon}
+                            source={require('../../assets/badge.png')}
+                        />
+                        <Image
+                            style={styles.badgeIcon}
+                            source={require('../../assets/badge.png')}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
+        </ImageBackground>
     );
 };
